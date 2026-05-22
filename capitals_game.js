fad5788ad7a1452018b7cf9371e04916b1e62949
Jisan Zaman <jzaman@wesleyan.edu>
2026-05-22 14:19:52 -0400

    added correct, wrong table, fixed multiple country answer display

--- capitals_game.js
+++ capitals_game.js
@@ -4,9 +4,7 @@ let score = 0;
 let countriesPlayed = [];
 let numCountries = Object.keys(capitals).length;
 
-export function checkAnswer() {
-  const answer = document.getElementById('answer').value.trim();
-  const country = document.getElementById('country').innerText;
+export function getCorrectAnswer(country, answer) {
   const correctCapital = capitals[country];
   let correctAnswer = correctCapital;
   if (Array.isArray(correctCapital)) {
@@ -15,17 +13,59 @@ export function checkAnswer() {
     console.log(`Correct capital: ${correctCapital}, User answer: ${answer}`);
     correctAnswer = correctCapital.toLowerCase() === answer.toLowerCase();
   }
+  return correctAnswer;
+}
+
+export function checkAnswer() {
+  const answer = document.getElementById('answer').value.trim();
+  const country = document.getElementById('country').innerText;
+  const correctCapital = capitals[country];
+  const correctAnswer = getCorrectAnswer(country, answer);
+  // let correctAnswer = correctCapital;
+  // if (Array.isArray(correctCapital)) {
+  //   correctAnswer = correctCapital.map((v) => v.toLowerCase()).includes(answer.toLowerCase());
+  // } else {
+  //   console.log(`Correct capital: ${correctCapital}, User answer: ${answer}`);
+  //   correctAnswer = correctCapital.toLowerCase() === answer.toLowerCase();
+  // }
   if (correctAnswer) {
     incrementScore();
   }
   countriesPlayed.push(country);
   document.getElementById('progress-value').innerText = countriesPlayed.length;
-  const correctAnswerText = `The capital of ${country} is ${correctCapital}`;
+  const correctCapitalText = Array.isArray(correctCapital) ? `one of these: ${correctCapital.join(', ')}` : `${correctCapital}`;
+  const correctAnswerText = `The capital of ${country} is ${correctCapitalText}`;
   const yourAnswerText = `Your answer: ${answer}`;
   document.getElementById('feedback').innerText = correctAnswer ? `Correct! ${correctAnswerText}.` : `Wrong! ${correctAnswerText}. ${yourAnswerText}`;
   document.getElementById('score').innerText = `Score: ${getScore()}`;
+  
+  // add the country and correct answer to the table
+  addCountryAnswerToHTML(country, answer);
+  // reset the answer input and play the next country
   document.getElementById('answer').value = '';
   return playGame();
+}
+
+export function addCountryAnswerToHTML(country, answer) {
+  const tbodyElem = document.getElementById('answers-body');
+  const trElem = document.createElement('tr');
+  const countryTd = document.createElement('td');
+  countryTd.innerText = country;
+  const capitalTd = document.createElement('td');
+  capitalTd.innerText = capitals[country];
+  trElem.appendChild(countryTd);
+  trElem.appendChild(capitalTd);
+  tbodyElem.appendChild(trElem);
+
+  const correctAnswer = getCorrectAnswer(country, answer);
+  trElem.style.backgroundColor = correctAnswer ? 'lightgreen' : 'lightcoral';
+  const resultTd = document.createElement('td');
+  resultTd.innerText = correctAnswer ? 'Correct' : 'Wrong';
+  trElem.appendChild(resultTd);
+
+  const yourAnswerTd = document.createElement('td');
+  yourAnswerTd.innerText = answer;
+  trElem.appendChild(yourAnswerTd);
 }
 
 export function getCapital(country) {
