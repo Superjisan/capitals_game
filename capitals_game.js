7de27eca39714e6e209732561b468329fc1a4a50
Jisan Zaman <jzthegreat@gmail.com>
2026-06-03 13:34:36 -0400

    add progressive web app service worker

--- capitals_game.js
+++ capitals_game.js
@@ -204,3 +204,10 @@ document.getElementById('oceania').addEventListener('click', () => {
   playGame();
 });
 
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/sw.js')
+      .then(reg => console.log('Service Worker registered!', reg))
+      .catch(err => console.log('Registration failed: ', err));
+  });
+}
